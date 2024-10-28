777ecf90b816ae272eab406c3e6e6683b453b051
Berndhura <wichura@gmx.de>
2024-10-28 13:45:09 +0100

    GET request with table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Conditional } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  results: any={};
+  books: any=[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {      
+    // Simple GET request with response type <any>
+    this.http.get<any>('https://api.npms.io/v2/search?q=scope:angular').subscribe(data => {
+        console.log(data)
+        this.results = data['results']
+        this.books = Object.values(this.results);
+    })
+}
   title = 'GithubList';
 }
